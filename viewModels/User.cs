a04a64cf15ce9836475da0cac2562325f28c0082
Dev.Pachon <54422629+Dev-Pachon@users.noreply.github.com>
2021-09-05 11:20:22 -0500

    Initial commit

---
+++ viewModels/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPCAuthenticationModule.viewModels
+{
+    public class User
+    {
+        [Required, StringLength(15, MinimumLength = 5), Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Compare("Password",ErrorMessage ="The password don't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required, Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "First Name")]
+        public string LastName { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
