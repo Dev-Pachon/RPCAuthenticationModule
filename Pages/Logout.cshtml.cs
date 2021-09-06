a7179fc9911220a721a4c25e78336d2646460eea
Dev.Pachon <54422629+Dev-Pachon@users.noreply.github.com>
2021-09-06 13:49:32 -0500

    Added the class Diagram in .svg format

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RPCAuthenticationModule.Pages
 {
+    [Authorize]
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<IdentityUser> signInManager;
